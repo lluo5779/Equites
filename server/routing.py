42ba66ecb02567cd2f6f7be05c207ba78f4054ae
miketravis <miketravis1203@gmail.com>
2019-11-17 15:24:50 -0500

    Working files are base1, About, 404, Contact, ContactSent, home, Option1, OptionDecision, portfolio

--- server/routing.py
+++ server/routing.py
@@ -1,8 +1,9 @@
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import login_user, logout_user, current_user, login_required
 
-
 # from app.forms import LoginForm, RegistrationForm
+from server.models.stock.stock import Stocks
+
 
 # Render home page
 # @app.route('/')
@@ -25,3 +26,69 @@ def login():
             next_page = url_for('index')
         return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
+
+
+def sender():
+    return render_template('sender.jinja2', title='Sign In')
+
+
+def receiver(fname, lname):
+    listA = ['a', 'b', 'c', 'd', 'ewrqrewafr']
+    return render_template('receiver.jinja2', title='Sign In', fname=fname, lname=lname, listA=listA)
+
+
+def optiondecision():
+    return render_template('OptionDecision.jinja2', title='optiondecision')
+
+
+def option1():
+    return render_template('Option1.jinja2', stock=Stocks())
+
+
+def option2():
+    return render_template('Option2.jinja2', stock=Stocks())
+
+
+def option3parent():
+    return render_template('Option3Parent.jinja2', title='optiondecision')
+
+def contact():
+    return render_template('Contact.jinja2', title='Contact Us')
+
+
+def option3childa():
+    return render_template('Option3ChildA.jinja2', title='optiondecision')
+
+
+def option3childb():
+    return render_template('Option3ChildB.jinja2', title='optiondecision')
+
+
+def option3childc():
+    return render_template('Option3ChildC.jinja2')
+
+def sendcontact():
+    return render_template('ContactSent.jinja2')
+
+def aboutus():
+    return render_template('About.jinja2')
+
+'''
+def portfoliio():
+    weightings = [0.6,0.4,0,0,0,0,0,0,0,0,0,0,0]
+    #initial user input
+    histPortValue = [1000,990,1050,1100]
+    histVol = 0.12
+    expectedReturn = 0.1
+    expectedVol = 0.1
+    risk = 'High'
+    return render_template('portfolio.jinja2', title='Sign In', weightings=weightings, risk=risk,histPortValue=histPortValue,histVOL=histVol, expectedReturn=expectedReturn,expectedVol=expectedVol)
+'''
+
+def portfolioview():
+    weightings = [0.6, 0.4]
+    #initial user input
+    expectedReturn = 0.1
+    expectedVol = 0.1
+    risk = 'High'
+    return render_template('portfolio.jinja2', title='Sign In', weightings=weightings, risk=risk, expectedReturn=expectedReturn, expectedVol=expectedVol)
---
+++ server/static/js/Nav.js
@@ -0,0 +1,26 @@
+document.write('\
+    <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #3B4F66;">\
+        <a class="navbar-brand" href="#">\
+            <img class="nav-logo" src="./imgs/Capstone logo.jpg" width="50" height="50" alt="Equites">\
+        </a>\
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">\
+            <span class="navbar-toggler-icon"></span>\
+          </button>\
+        <div class="collapse navbar-collapse" id="navbar-text">\
+            <ul class="navbar-nav mr-auto">\
+                <li class="nav-item">\
+                    <a class="nav-link">About</a>\
+                </li>\
+                <li class="nav-item">\
+                    <a class="nav-link">Contact Us</a>\
+                </li>\
+            </ul>\
+            <form class="form-inline my-2 my-lg-0">\
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Login</button>\
+            </form>\
+        </div>\
+    </nav>\
+    \
+    \
+    \
+    ');
