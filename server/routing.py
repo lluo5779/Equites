78c618840a6c52cb2692604689acb24917e433d3
miketravis <miketravis1203@gmail.com>
2019-11-18 11:41:50 -0500

    fixed href links in base.hmtl and changed extend to base.html in contact, contactsent, about and home

--- server/routing.py
+++ server/routing.py
@@ -24,7 +24,7 @@ def login():
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('index')
         return redirect(next_page)
-    return render_template('login.html', title='Sign In', form=form)
+    return render_template('login.jinja2', title='Sign In', form=form)
 
 
 def sender():
